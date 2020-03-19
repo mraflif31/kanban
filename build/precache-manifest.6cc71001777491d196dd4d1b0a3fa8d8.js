6410ef03b2e1bdb57bf6804f39e7dfc7c39cd090
M Rafli Fadillah <mraflif31@yahoo.com>
2020-03-19 09:33:48 +0700

    2nd commit

---
+++ build/precache-manifest.6cc71001777491d196dd4d1b0a3fa8d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d62742f788de3f43b3da152a594438",
+    "url": "/kanban/index.html"
+  },
+  {
+    "revision": "0ca2812e9b00794714d9",
+    "url": "/kanban/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7be53158137f2bec3d50",
+    "url": "/kanban/static/css/main.219d565a.chunk.css"
+  },
+  {
+    "revision": "0ca2812e9b00794714d9",
+    "url": "/kanban/static/js/2.0984e78b.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/kanban/static/js/2.0984e78b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be53158137f2bec3d50",
+    "url": "/kanban/static/js/main.143e20ab.chunk.js"
+  },
+  {
+    "revision": "2641a4aa5867993d4310",
+    "url": "/kanban/static/js/runtime-main.a76e7e4a.js"
+  }
+]);
